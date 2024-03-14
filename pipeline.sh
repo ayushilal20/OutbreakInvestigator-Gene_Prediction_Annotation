ed04df402b5b28c754599bd5a0c91507ecb4cfac
Johns, Lucy <ljohns9@gatech.edu>
2024-03-14 11:52:25 -0400

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -2,30 +2,30 @@
 source ~/.bash_profile
 
 #env for prodigal
-conda create -n gene_pred
-conda activate gene_pred
-conda install -c bioconda prodigal
-conda deactivate
+# conda create -n gene_pred
+# conda activate gene_pred
+# conda install -c bioconda prodigal
+# conda deactivate
 
 # env for bakta
-conda create -n bakta
-conda activate bakta
-conda install -c conda-forge -c bioconda bakta
-bakta_db download --output <output-path> --type full
-conda deactivate 
+# conda create -n bakta
+# conda activate bakta
+# conda install -c conda-forge -c bioconda bakta
+# bakta_db download --output <output-path> --type full
+# conda deactivate 
 
 # env for amrfinder
-conda create -n amrfinder
-conda activate amrfinder
-mamba install -c bioconda ncbi-amrfinderplus
-conda deactivate
+# conda create -n amrfinder
+# conda activate amrfinder
+# mamba install -c bioconda ncbi-amrfinderplus
+# conda deactivate
 
 #env for pathofact
-git lfs install
-git clone -b master --recursive https://git-r3lab.uni.lu/laura.denies/PathoFact.git
-cd PathoFact
-conda env create -f=envs/PathoFact.yaml
-cd ..
+# git lfs install
+# git clone -b master --recursive https://git-r3lab.uni.lu/laura.denies/PathoFact.git
+# cd PathoFact
+# conda env create -f=envs/PathoFact.yaml
+# cd ..
 
 # commandline usage: sh pipeline.sh [input_dir] [output_dir]
 input_dir="$1"
@@ -53,6 +53,7 @@ for file in "$input_dir"/*; do
 	conda activate bakta
 	bakta --db ~/bakta/db/ \
 	"$isolate/filtered_contigs.fa" \
+	--output "$output_dir/bakta/bakta_output" \
 	--threads 8 \
 	> "$output_dir/bakta/bakta_output.log" \
 	2> "$output_dir/bakta/bakta_error.log"
